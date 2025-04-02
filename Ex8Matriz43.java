56da6044c7445fcbf48ab65c0830b1371bf9b147
IsaStev <isabela.alves08.fatec@gmail.com>
2025-04-02 19:30:05 -0300

    Update Ex8Matriz43.java

--- Ex8Matriz43.java
+++ Ex8Matriz43.java
@@ -1,56 +1,62 @@
 package com.mycompany.aulavetores;
-
 import javax.swing.JOptionPane;
 
-public class Ex8Matriz43 {
-    public static void main(String[] args) {
-        int vendas[][] = new int[4][3]; // 4 semanas x 3 produtos
+public  class Ex8Matriz43{
+    public static void main (String [] args){
+    
+        int vendas[][] = new int [4][3];
         int i, j;
-        String resultado = "";
-
-        // Preenchendo a matriz com entrada do usuário
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 3; j++) {
-                vendas[i][j] = Integer.parseInt(JOptionPane.showInputDialog(
-                        "Digite a quantidade vendida do produto " + (j + 1) + " na semana " + (i + 1) + ":"));
-            }
-        }
-
-        // a. Quantidade de cada produto vendido no mês
-        int totalProduto[] = new int[3]; 
-        for (j = 0; j < 3; j++) {
-            for (i = 0; i < 4; i++) {
-                totalProduto[j] += vendas[i][j];
-            }
-        }
-
-        resultado += "Quantidade total vendida por produto no mês:\n";
-        for (j = 0; j < 3; j++) {
-            resultado += "Produto " + (j + 1) + ": " + totalProduto[j] + "\n";
-        }
-
-        // b. Quantidade de produtos vendidos por semana
-        int totalSemana[] = new int[4]; 
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 3; j++) {
-                totalSemana[i] += vendas[i][j];
-            }
-        }
-
-        resultado += "\nQuantidade total vendida por semana:\n";
-        for (i = 0; i < 4; i++) {
-            resultado += "Semana " + (i + 1) + ": " + totalSemana[i] + "\n";
-        }
-
-        // c. Total de produtos vendidos no mês
+        int totalProdutos[] = new int[3];
+        int totalSemanas[] = new int[4];
         int totalMes = 0;
-        for (i = 0; i < 4; i++) {
-            totalMes += totalSemana[i];
+        String resultado = "";
+        
+        
+        //Entrada de dados
+        for (i = 0; i < 4; i++){
+            for(j = 0; j <3; j++){
+                
+                vendas[i][j] = Integer.parseInt(JOptionPane.showInputDialog ("Digite a quantidade vendida do produto " + (j+1) + " na semana " + (i+1) + ":"));
+            }
         }
-
-        resultado += "\nTotal de produtos vendidos no mês: " + totalMes;
-
-        // Exibir resultado
+        
+        //Quantodade ventida por produto
+        for(j = 0; j < 3; j++){
+            for(i = 0; i < 4; j++){
+                totalProdutos[j] = totalProdutos[j] + vendas[i][j];
+                
+            }
+        }
+        
+        resultado = resultado + "Quantidade total vendida por produto no mes: \n";
+        for(j = 0; j < 3; j++){
+            resultado = resultado + "Produto " + (j + 1) + ": " + totalProdutos[j] + "\n";
+            
+        }
+    
+        //Quantodade vendida por semana
+        for (i = 0; i < 4; i++){
+            for(j = 0; j < 3; j++){
+                totalSemanas[i] = totalSemanas[i] + vendas[i][j];
+                
+            }
+        }
+        
+        resultado = resultado + "Quantidade total vendida por semana no mes: \n";
+        for(i = 0; i < 4; i++){
+            resultado = resultado + "Semana " + (i + 1) + ": " + totalSemanas[i] + "\n";
+            
+        }
+        
+        //Total mes
+        for (i = 0; i< 4; i ++){
+            totalMes = totalMes + totalSemanas[i];
+            
+        }
+        
+        resultado = resultado + "\n Total de produtos vendidos no mês: " + totalMes;
+        
         JOptionPane.showMessageDialog(null, resultado);
+        
     }
 }
